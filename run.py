c42842de854cf89bf9bfe6da8ab71ba88b9f6cfc
BelT26 <helen.taylor@hotmail.it>
2021-07-14 13:00:35 +0000

    calculate stock required and add to stock worksheet

--- run.py
+++ run.py
@@ -80,6 +80,19 @@ def get_last_five_entries_sales():
     return columns    
 
 
+def calculate_stock_data(data):
+    """calculate average from data passed in and add 10%"""
+    print('Calculating stock data...\n')
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
+
+
 def main():
     """ run main program functions"""
     data = get_sales_data()
@@ -89,8 +102,10 @@ def main():
     new_surplus_data = calculate_surplus(sales_data)
     print(new_surplus_data)
     update_worksheet(new_surplus_data, 'surplus')
-
+    sales_columns = get_last_five_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    print(stock_data)
+    update_worksheet(stock_data, 'stock')
 
 print('Welcome to Love Sandwiches data automation')
-# main()"""
-sales_columns = get_last_five_entries_sales()
+main()
