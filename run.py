5370a2dedb1cef25b0a573e27922d85be1c92890
BelT26 <helen.taylor@hotmail.it>
2021-07-14 09:04:46 +0000

    Add validation check for number of entries

--- run.py
+++ run.py
@@ -10,17 +10,33 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json') 
 SCOPED_CREDS = CREDS.with_scopes(SCOPE) 
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS) 
-SHEET = GSPREAD_CLIENT.open('love_sandwiches') 
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
     get sales figures from user
     """
+
     print('Please enter sales data from the last market day')
     print('Data should be six numbers separated by commas')
     print('For example: 1,3,6,4,12,5\n')
-
     data_str = input('Enter your data here:')
-    print(f'The data provided is {data_str}')
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
 
-get_sales_data()    
+
+def validate_data(values):
+    """
+    inside the try check that all inputs can be converted to integers
+    raise a ValueError if value cannot be parsed or
+    if there are not exactly 6 numbers
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(f'6 values expected.  You provided {len(values)}')
+    except ValueError as e:
+        print(f'Invalid data {e}. Please try again')    
+
+  
+get_sales_data()
